a233f9e8517492512c21b842b042ad04de8e483f
DapperMink <quentinjanuelkij@gmail.com>
2022-03-15 13:50:35 +0100

    fullscreen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,49 +5,67 @@ import { getExternal, load, Inputs } from "../gm8emu";
 
 import styles from "./index.module.scss";
 
+enum State {
+	Playing,
+	Ready,
+	NotReady,
+}
+
+const resizeCanvas = function (canvas: HTMLCanvasElement) {
+	const ratio = canvas.width / canvas.height;
+	const cssDimensions = {
+		x: window.innerWidth,
+		y: window.innerHeight,
+	};
+	if (window.innerWidth / window.innerHeight > ratio)
+		cssDimensions.x = window.innerHeight * ratio;
+	else
+		cssDimensions.y = window.innerWidth / ratio;
+	canvas.style.width = `${ cssDimensions.x }px`;
+	canvas.style.height = `${ cssDimensions.y }px`;
+	const left = (window.innerWidth - cssDimensions.x) / 2;
+	const top = (window.innerHeight - cssDimensions.y) / 2;
+	canvas.style.left = `${ left }px`;
+	canvas.style.top = `${ top }px`;
+};
+
 export const App = function () {
 	const canvas = React.useRef<HTMLCanvasElement>(null);
+	const [state, setState] = React.useState(State.NotReady);
 	const [ctx, setCtx] = React.useState<CanvasRenderingContext2D>();
-	const [ready, setReady] = React.useState(false);
 	useEffect(() => {
-		if (!ctx)
+		if (!ctx || !canvas.current)
 			return;
-		if (ready)
+		if (state != State.NotReady)
 			return;
 		init(getExternal(ctx));
-		setReady(true);
+		window.addEventListener("keydown", e => Inputs.onKeyDown(e.key));
+		window.addEventListener("keyup", e => Inputs.onKeyUp(e.key));
+		window.addEventListener("resize", () => resizeCanvas(canvas.current!));
+		resizeCanvas(canvas.current);
+		setState(State.Ready);
 	}, [ctx]);
 	useEffect(() => {
 		setCtx(canvas.current?.getContext("2d") ?? undefined);
 	}, [canvas]);
-	// useEffect(() => {
-	// 	if (!ctx)
-	// 		return;
-	// 	const array = new Uint8ClampedArray(texture);
-	// 	const image = new ImageData(array, array.length / 4 / 480);
-	// 	ctx.putImageData(image, 0, 0);
-	// }, [ctx]);
 	return <div className={ styles.container }>
-		<button
-			onClick={ () => {
-				(async () => {
-					if (!ready)
-						return;
-					const data = await load();
-					console.log("Running...");
-					console.log(await run(data));
-				})()
-				.catch(console.error);
-			} }
-		>Load file</button>
+		{ state == State.Ready ? <button
+				onClick={ () => {
+					(async () => {
+						const data = await load();
+						console.log("Running...");
+						setState(State.Playing);
+						console.log(await run(data));
+					})()
+					.catch(console.error);
+				} }
+			>Load assets</button> : null
+		}
 		<canvas
 			ref={ canvas }
 			width={ 800 }
 			height={ 608 }
-			style={ { border: "1px solid black" } }
-			onKeyDown={ e => Inputs.onKeyDown(e.key) }
-			onKeyUp={ e => Inputs.onKeyUp(e.key) }
-			tabIndex={ 0 }
+			hidden={ state != State.Playing }
 		></canvas>
 	</div>;
 }
